3f6b9619530df48ccfc6870aae7920f1230756f0
Andrew Bartsch <123200636+AndrewBartsch42@users.noreply.github.com>
2026-04-28 18:39:40 -0500

    everything should work as mostly intended

--- public/app.js
+++ public/app.js
@@ -11,21 +11,32 @@ async function loadChampion(){
         const champion_details = await response.json();
         document.getElementById("champion_portrait").src = `https://ddragon.leagueoflegends.com/cdn/16.8.1/img/champion/${championSelector}.png`;
         document.getElementById("championName").textContent = champion_details.name;
-        //document.getElementById("passive_img").src = champion_details.data.championSelector.image.sprite;
+        //champ passive
+        const championPassive = champion_details.data[championSelector].passive.image.full;
+        document.getElementById("passive_img").src = `https://ddragon.leagueoflegends.com/cdn/16.8.1/img/passive/${championPassive}`;
         document.getElementById("passive_name").textContent = champion_details.data[championSelector].passive.name;
-        //document.getElementById("passive_description").textContent = champion_details.data.passive.description;
-        //document.getElementById("ability_1_img").src = champion_details.spells[0].image.sprite;
-        //document.getElementById("ability_1_name").textContent = champion_details.spells[0].name;
-        //document.getElementById("abiltiy_1_description").textContent = champion_details.spells[0].tooltip;
-        //document.getElementById("ability_2_img").src = champion_details.spells[1].image.sprite;
-        //document.getElementById("ability_2_name").textContent = champion_details.spells[1].name;
-        //document.getElementById("abiltiy_2_description").textContent = champion_details.spells[1].tooltip;
-        //document.getElementById("ability_3_img").src = champion_details.spells[2].image.sprite;
-        //document.getElementById("ability_3_name").textContent = champion_details.spells[2].name;
-        //document.getElementById("abiltiy_3_description").textContent = champion_details.spells[2].tooltip;
-        //document.getElementById("ability_4_img").src = champion_details.spells[3].image.spritel;
-        //document.getElementById("ability_4_name").textContent = champion_details.spells[3].name;
-        //document.getElementById("abiltiy_4_description").textContent = champion_details.spells[3].tooltip;
+        document.getElementById("passive_description").textContent = champion_details.data[championSelector].passive.description;
+        //champ ability 1
+        const championAbility_1 = champion_details.data[championSelector].spells[0].image.full;
+        document.getElementById("ability_1_img").src = `https://ddragon.leagueoflegends.com/cdn/16.8.1/img/spell/${championAbility_1}`;
+        document.getElementById("ability_1_name").textContent = champion_details.data[championSelector].spells[0].name;
+        document.getElementById("ability_1_description").textContent = champion_details.data[championSelector].spells[0].description;
+        //champ ability 2
+        const championAbility_2 = champion_details.data[championSelector].spells[1].image.full;
+        document.getElementById("ability_2_img").src = `https://ddragon.leagueoflegends.com/cdn/16.8.1/img/spell/${championAbility_2}`;
+        document.getElementById("ability_2_name").textContent = champion_details.data[championSelector].spells[1].name;
+        document.getElementById("ability_2_description").textContent = champion_details.data[championSelector].spells[1].description;
+        //champ ability 3
+        const championAbility_3 = champion_details.data[championSelector].spells[2].image.full;
+        document.getElementById("ability_3_img").src = `https://ddragon.leagueoflegends.com/cdn/16.8.1/img/spell/${championAbility_3}`;
+        document.getElementById("ability_3_name").textContent = champion_details.data[championSelector].spells[2].name;
+        document.getElementById("ability_3_description").textContent = champion_details.data[championSelector].spells[2].description;
+        //champ ability 4
+        const championAbility_4 = champion_details.data[championSelector].spells[3].image.full;
+        document.getElementById("ability_4_img").src = `https://ddragon.leagueoflegends.com/cdn/16.8.1/img/spell/${championAbility_4}`;
+        document.getElementById("ability_4_name").textContent = champion_details.data[championSelector].spells[3].name;
+        document.getElementById("ability_4_description").textContent = champion_details.data[championSelector].spells[3].description;
+        //remove the hidden tag
         details.classList.remove("hidden");
     } catch (error) {
         console.log(error);
